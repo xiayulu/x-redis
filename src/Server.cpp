21f52cdc4559f4f214988b8bf165345655878429
xiayulu <harolddfinchh@gmail.com>
2024-11-08 18:30:30 +0800

    zu2

--- src/Server.cpp
+++ src/Server.cpp
@@ -9,6 +9,22 @@
 #include <thread>
 #include <unistd.h>
 
+void handle_request(int client_fd) {
+
+  char *request[100];
+
+  while (recv(client_fd, (void *)request, 100, 0) != -1) {
+    // got request msg
+    const char *response = "+PONG\r\n";
+    if (send(client_fd, (const void *)response, strlen(response), 0) == -1) {
+      std::cerr << "Failed to send message\n";
+      return;
+    }
+  }
+
+  std::cout << "No message recieved, client " << client_fd << " closed\n";
+}
+
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
@@ -55,34 +71,24 @@ int main(int argc, char **argv) {
   int client_addr_len = sizeof(client_addr);
 
   std::cout << "Waiting for a client to connect...\n";
-  // Accept a connection from a client
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                         (socklen_t *)&client_addr_len);
-  if (client_fd < 0) {
-    std::cerr << "accept failed\n";
-    return 1;
-  }
-  std::cout << "Client connected\n";
 
-  char *msg[100];
-
-  while (recv(client_fd, (void *)msg, 100, 0) != -1) {
-    // got request msg
-
-    const char *message = "+PONG\r\n";
-    if (send(client_fd, (const void *)message, strlen(message), 0) == -1) {
-
-      std::cerr << "Failed to send message\n";
-
+  // handle client requests
+  while (true) {
+    // Accept a connection from a client
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (socklen_t *)&client_addr_len);
+    if (client_fd < 0) {
+      std::cerr << "accept failed\n";
       return 1;
     }
-  }
+    std::cout << "Client " << client_fd << " connected\n";
 
-  std::cout << "No message recieved, client closed\n";
+    std::thread th(handle_request, client_fd);
+    th.detach();
+  }
 
   // close
   close(server_fd);
-  close(client_fd);
 
   return 0;
 }
